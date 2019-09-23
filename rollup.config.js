c18723f3223c58b4b3df7dca6a29a8dda7cc4ec9
Dung Trann <dung.trann@groovetechnology.com>
2019-09-23 15:20:06 +0700

    disable mjs build

--- rollup.config.js
+++ rollup.config.js
@@ -7,7 +7,7 @@ import pkg from './package.json';
 export default {
   input: 'src/index.tsx',
   output: [
-    { file: pkg.module, format: 'es' },
+    // { file: pkg.module, format: 'es' },
     { file: pkg.main, format: 'cjs' },
   ],
   plugins: [
@@ -20,7 +20,7 @@ export default {
       extensions: ['.js', '.jsx', '.ts', '.tsx'],
     }),
     commonjs({
-      extensions: ['.js', '.mjs'],
+      extensions: ['.js'],
       include: 'node_modules/**',
     }),
     postcss({
@@ -30,7 +30,7 @@ export default {
     }),
     babel({
       exclude: 'node_modules/**',
-      extensions: ['.mjs', '.js', '.jsx', '.ts', '.tsx'],
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
     }),
   ],
 };
